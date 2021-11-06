0e49bbed7e600b4cc5791d5b0c95ab3af4f3c71f
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2021-11-05 23:06:35 -0400

    bump version

--- version.php
+++ version.php
@@ -37,7 +37,7 @@ DM20-0196
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2021110501;
+$plugin->version = 2021110502;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
@@ -54,4 +54,4 @@ $plugin->dependencies = [
 $plugin->maturity = MATURITY_BETA;
 
 // This is the named version.
-$plugin->release = '0.1.1';
+$plugin->release = '0.1.2';
