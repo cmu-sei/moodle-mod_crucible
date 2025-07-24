3070bcc727f536196e255ed13ba32a2ea843f8b5
Nuria Pacheco <npacheco@sei.cmu.edu>
2025-07-24 12:16:03 -0400

    moodle version fix

--- version.php
+++ version.php
@@ -45,7 +45,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2025072400;
 
 // This is the version of Moodle this plugin requires.
-$plugin->requires = 2025041401;
+$plugin->requires = 2025041400;
 
 // This is the component name of the plugin - it always starts with 'component_'.
 $plugin->component = 'mod_crucible';
