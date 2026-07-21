b3d3732e4ce56f517677123efc872a6254b6811a
Austin Vershave <130477170+sei-avershave@users.noreply.github.com>
2026-07-21 10:15:58 -0400

    Fixes Alloy timer to not show expired before the event is deployed (#31)
    Co-authored-by: Austin M Vershave <amvershave@cert.org>

--- version.php
+++ version.php
@@ -42,7 +42,7 @@ DM20-0196
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2026070805;
+$plugin->version = 2026071700;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2025041400;
